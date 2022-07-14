78735e08a407b7ca5006dd383272f8f04bc6d2f8
Victor-pixel98 <75060742+Victor-pixel98@users.noreply.github.com>
2022-07-14 13:51:11 +0600

    Full verstka
    Сверстал весь сайт кроме последнего чекбокса
    Остался адаптив

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 $(function () {
-    $('.header__btn').on('click',function(){
+    $('.header__btn').on('click', function () {
         $('.rightside-menu').removeClass('rightside-menu--close')
     });
-    $('.rightside-menu__close').on('click',function(){
+    $('.rightside-menu__close').on('click', function () {
         $('.rightside-menu').addClass('rightside-menu--close')
     });
 
@@ -12,6 +12,18 @@ $(function () {
         fade: true,
         autoplay: true,
     });
+    $('.contact-slider').slick({
+        slidesToShow: 10,
+        slidesToScroll: 10,
+        dots: true,
+        arrows: false,
+    });
+    $('.article-slider__box').slick({
+        prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/blog/arrow-slide-left.svg"></button>',
+        nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="images/blog/arrow-slide-right.svg"></button>',
+    });
+
+
 
     var mixer = mixitup('.gallery__inner', {
         load: {
@@ -19,3 +31,5 @@ $(function () {
         }
     });
 })
+
+
