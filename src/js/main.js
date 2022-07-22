d2d7c412f5d9b2727bad0303aa985316579bbcac
Victor-pixel98 <75060742+Victor-pixel98@users.noreply.github.com>
2022-07-22 13:17:08 +0600

    сверстал фулл сайт
    и адаптив

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,14 @@ $(function () {
         $('.rightside-menu').addClass('rightside-menu--close')
     });
 
+    $('.header__btn-menu ').on('click', function () {
+        $('.menu').toggleClass('menu--open')
+    });
+
+    if ($(window).width() < 651) {
+        $('.works-path__item--measuring').appendTo($('.works-path__item-box'));
+    }
+
     $('.top__slider').slick({
         dots: true,
         arrows: false,
@@ -17,19 +25,64 @@ $(function () {
         slidesToScroll: 10,
         dots: true,
         arrows: false,
+        responsive: [
+            {
+                breakpoint: 1700,
+                settings: {
+                    slidesToScroll: 8,
+                    slidesToShow: 8,
+                }
+            },
+            {
+                breakpoint: 1511,
+                settings: {
+                    slidesToScroll: 6,
+                    slidesToShow: 6,
+                }
+            },
+            {
+                breakpoint: 1201,
+                settings: {
+                    slidesToScroll: 4,
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 841,
+                settings: {
+                    slidesToScroll: 3,
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 551,
+                settings: {
+                    slidesToScroll: 2,
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 376,
+                settings: {
+                    slidesToScroll: 1,
+                    slidesToShow: 1,
+                }
+            },
+        ]
+        
     });
-    $('.article-slider__box').slick({
-        prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/blog/arrow-slide-left.svg"></button>',
-        nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="images/blog/arrow-slide-right.svg"></button>',
-    });
+$('.article-slider__box').slick({
+    prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/blog/arrow-slide-left.svg"></button>',
+    nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="images/blog/arrow-slide-right.svg"></button>',
+});
 
 
 
-    var mixer = mixitup('.gallery__inner', {
-        load: {
-            filter: '.living'
-        }
-    });
+var mixer = mixitup('.gallery__inner', {
+    load: {
+        filter: '.living'
+    }
+});
 })
 
 
