989faac0ba6f1b4261a00d9a892951fb6fd2ca3b
Victor-pixel98 <75060742+Victor-pixel98@users.noreply.github.com>
2022-07-25 12:14:29 +0600

    1
    1

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,14 @@
 $(function () {
     $('.header__btn').on('click', function () {
         $('.rightside-menu').removeClass('rightside-menu--close')
+       
     });
     $('.rightside-menu__close').on('click', function () {
         $('.rightside-menu').addClass('rightside-menu--close')
+        
     });
+
+
 
     $('.header__btn-menu ').on('click', function () {
         $('.menu').toggleClass('menu--open')
